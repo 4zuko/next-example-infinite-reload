47eb575699f376958af19963171c27d435bac89b
Anis Zuko <aniszuko@gmail.com>
2023-09-14 15:53:06 +0200

    chore: How to reproduce the issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
+const BASE_PATH = '/base-path'
+const ASSET_PREFIX = 'asset-prefix'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: { basePath: BASE_PATH },
+  output: 'standalone',
+  basePath: BASE_PATH,
+  assetPrefix: `${BASE_PATH}/${ASSET_PREFIX}`,
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: `/:locale/${ASSET_PREFIX}/_next/:path*`,
+          destination: '/_next/:path*',
+          locale: false,
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig
